6cba0c53d07a20c39282c630783bb4737c140584
LAPTOP-3C3QG40E\Nindya Novitasari <nindyanovitasari90@gmail.com>
2020-03-18 16:09:02 +0700

    tmplt daftar

--- application/views/pages/pendaftaran/index.php
+++ application/views/pages/pendaftaran/index.php
@@ -1,4 +1,4 @@
- <section class="page-cover" id="cover-about-us">
+ <section class="section section-breadcrumbs" id="cover-about-us">
             <div class="container">
                 <div class="row">
                     <div class="col-sm-12">
